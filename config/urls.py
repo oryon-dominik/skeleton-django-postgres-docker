e93d02930c0e9ccef831a711808f45b7afea8a35
Dominik Geldmacher <oryon@cyberise.de>
2020-04-15 14:46:29 +0200

    Initial commit

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+""" base URL Configuration
+
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html')),
+    path('', include('apps.users.urls', namespace="users")),
+    path('', include('apps.things.urls', namespace="things")),
+    path('admin/', admin.site.urls),
+]
